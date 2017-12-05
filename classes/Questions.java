e21ca5da79c3e26aff2d3b06313a7d832a4acfb5
Joshua Hergenroeder 4944 <dgic@fresnou.org>
2017-12-05 14:36:50 -0800

    End of 05/12/17
    Started A4 Excercise 1

---
+++ classes/Questions.java
@@ -0,0 +1,31 @@
+/**
+ * Write a description of class Questions here.
+ *
+ * @author Joshua Hergenroeder
+ * @version 04 December 2017
+ */
+public class Questions
+{
+    /* QUESTIONS
+    * A1
+       * 1) It takes 8 bits to represent 255 in binary.
+       * 2) It takes 3 bytes to represent a rgb color. One for each part.
+       * 3) There are 307,200 pixels in a 640 x 480 screen.
+    * A2
+       * 1) You combined red and blue mainly with a little bit of green. (255,102,255)
+       * 2) You combine red and green. (255,255,0)
+       * 3) You combine red and blue. (102,0,102)
+       * 4) You combine all of the colors (red, green, and blue) to their maximum. (255,255,255)
+       * 5) You combine all of the colors at a lower value. (102,102,102)
+    * A3
+       * 1) 0
+       * 2) 0
+       * 3) 640
+       * 4) 480
+       * 5) Top to bottom
+       * 6) Left to right
+       * 7) Yes. 
+    * A5
+       * 
+    */
+}
